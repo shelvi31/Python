e29d9095eee8eee5c2f162292c90645b08749be2
shelvi31 <garg.shelvi@gmail.com>
2020-05-28 08:02:18 +0530

    Code with Harry

---
+++ Code with Harry/Apni Dictionary.py
@@ -0,0 +1,8 @@
+d1={}
+print(type(d1))
+d1={"forgive":"maaf kar dena","forget":"bhul jana","bold":"veer","coward":"kayar","anger":"gussa"}
+print(d1)
+print("choose the word you want to find meaning for")
+str= "a"
+a = input()
+print(d1[a])
---
+++ Code with Harry/Lists and Functions.py
@@ -0,0 +1,20 @@
+grocery=["milk", "chai" , "coffee" , "dudh"]
+print(grocery[3])
+print(grocery)
+numbers=[4,5,1,7,8,0]
+numbers.sort()
+numbers.reverse()
+numbers.remove(4)
+numbers.pop()
+print(numbers)
+print(min(numbers))
+print(max(numbers))
+numbers[3]=5
+#tuple== immutable
+tp=(3,4,5)
+a=3
+b=7
+a,b= b,a
+print("a=",a)
+print("b=",b)
+
