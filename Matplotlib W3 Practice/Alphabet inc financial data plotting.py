5b44b79864e3258172127c6f4090b8c251bdc2e6
shelvi31 <garg.shelvi@gmail.com>
2020-09-22 07:59:24 +0530

    learning python

--- Matplotlib W3 Practice/Alphabet inc financial data plotting.py
+++ Matplotlib W3 Practice/Alphabet inc financial data plotting.py
@@ -1,11 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy
+import pandas as pd
 
-plt.figure(figsize=(4,10))
-plt.plot([1,2,3],[2,4,1])
-plt.xlabel("OCT 2016")
-plt.ylabel("open")
+print(plt.style.available)
+
+# plt.figure(figsize=(10,10))
+# df = pd.read_csv("fdata.csv")
+# df.plot()
+# plt.xlabel("OCT 2016")
+# plt.ylabel("achievements")
+# plt.show()
+
+month = ["Jan","Feb","March","April"]
+sales =[34,23,42,11]
+plt.plot(month,sales,"b--o")
+plt.tight_layout()
+plt.grid()
 plt.show()
+
 
 
 
---
+++ Python Practice/sortingByStringLength.py
@@ -0,0 +1,3 @@
+def sort_by_length(lst):
+	return sorted(lst,key=len)
+
